8ec1de9c4fbce6d4c58e154e0bfa20c5f3f049b1
bmaguir <bmaguir@tcd.ie>
2013-03-08 14:56:44 +0000

    New version as of week 8

--- src/com/android/maps/MapActivity.java
+++ src/com/android/maps/MapActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
